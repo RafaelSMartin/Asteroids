8bd98f3ac7061f18a0a2ff148a134a28490e9737
RafaelSMartin <rafaels.martin.dev@gmail.com>
2017-01-02 18:36:36 +0100

    Boton Acerca de...

---
+++ app/src/main/java/com/rafaels/asteroides/AcercaDe.java
@@ -0,0 +1,17 @@
+package com.rafaels.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Rafel S. Martin on 02/01/2017.
+ */
+
+public class AcercaDe extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+    }
+}
--- app/src/main/java/com/rafaels/asteroides/MainActivity.java
+++ app/src/main/java/com/rafaels/asteroides/MainActivity.java
@@ -1,13 +1,31 @@
 package com.rafaels.asteroides;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bAcercaDe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bAcercaDe = (Button) findViewById(R.id.Button03);
+        bAcercaDe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lanzarAcercaDe(null);
+            }
+        });
+    }
+
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDe.class);
+        startActivity(i);
     }
 }
