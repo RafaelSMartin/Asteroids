7bd7cd9609113623f6abde4ad2423066c070d6e4
RafaelSMartin <rafaels.martin.dev@gmail.com>
2017-01-02 18:46:54 +0100

    Boton Salir mediante un escuchador por codigo

--- app/src/main/java/com/rafaels/asteroides/MainActivity.java
+++ app/src/main/java/com/rafaels/asteroides/MainActivity.java
@@ -1,14 +1,15 @@
 package com.rafaels.asteroides;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private Button bAcercaDe;
+    private Button bAcercaDe, bSalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +18,15 @@ public class MainActivity extends AppCompatActivity {
 
         bAcercaDe = (Button) findViewById(R.id.Button03);
         bAcercaDe.setOnClickListener(new View.OnClickListener() {
-            @Override
             public void onClick(View view) {
                 lanzarAcercaDe(null);
+            }
+        });
+
+        bSalir = (Button) findViewById(R.id.Button04);
+        bSalir.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                finish();
             }
         });
     }
