1465d5394665688877b2950ffe1374e1527ab744
RafaelSMartin <rafaels.martin.dev@gmail.com>
2017-01-06 11:04:00 +0100

    Menu

--- app/src/main/java/com/rafaels/asteroides/MainActivity.java
+++ app/src/main/java/com/rafaels/asteroides/MainActivity.java
@@ -2,8 +2,10 @@ package com.rafaels.asteroides;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -34,5 +36,24 @@ public class MainActivity extends Activity {
     public void lanzarAcercaDe(View view){
         Intent i = new Intent(this, AcercaDe.class);
         startActivity(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        if(id == R.id.acercaDe){
+            lanzarAcercaDe(null);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
